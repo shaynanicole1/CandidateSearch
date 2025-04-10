ff7f069c43c55e9c162ab486d5aec33ac152b19d
shaynanicole1 <161741584+shaynanicole1@users.noreply.github.com>
2025-04-10 14:29:55 -0400

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // optional: disables chunk splitting
+      },
+    },
+  },
+});
